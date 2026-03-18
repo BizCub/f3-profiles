1fec0b06826c1aa3201a4f5283838ef6741a2c59
BizCub <bizcubmine@gmail.com>
2026-03-18 08:18:10 +0200

    The loader has been updated

---
+++ src/main/java/com/bizcub/debugOptionProfiles/platform/Forge.java
@@ -0,0 +1,9 @@
+//? forge {
+/*package com.bizcub.debugOptionProfiles.platform;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Forge.MOD_ID)
+public class Forge {
+    public static final String MOD_ID = /^$ mod_id^/ "debug_option_profiles";
+}*///?}
--- src/main/java/com/bizcub/debugOptionProfiles/platforms/Forge.java
+++
@@ -1,9 +0,0 @@
-//? forge {
-/*package com.bizcub.debugOptionProfiles.platforms;
-
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(Forge.modId)
-public class Forge {
-    public static final String modId = /^$ mod_id^/ "debug_option_profiles";
-}*///?}
