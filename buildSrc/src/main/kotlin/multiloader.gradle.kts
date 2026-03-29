c969eb163794d920ed22e9b4e9bc22da13dba515
BizCub <bizcubmine@gmail.com>
2026-03-29 11:57:37 +0200

    The loader has been updated

--- buildSrc/src/main/kotlin/multiloader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader.gradle.kts
@@ -1,25 +1,6 @@
 plugins {
-    id("multiloader-common")
     id("me.modmuss50.mod-publish-plugin")
+    id("com.bizcub.multiloader")
 }
 
-reps.clear()
-deps.clear()
-
-if (isFabric) {
-    deps.add(Dependency("net.fabricmc:fabric-loader:latest.release", "implementation"))
-    deps.add(Dependency("net.fabricmc.fabric-api:fabric-api:${getProp("fabric_api")}", "implementation"))
-}
-
-if (isNeoForge) {
-    reps.add(Repository("https://maven.neoforged.net/releases"))
-}
-
-sc.replacements {
-    string(scp >= "26.1") {
-        replace(".render(", ".extractRenderState(")
-        replace(".drawString(", ".text(")
-        replace("renderContent", "extractContent")
-        replace("GuiGraphics", "GuiGraphicsExtractor")
-    }
-}
+multiloader.init()
