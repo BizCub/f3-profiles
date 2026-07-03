ee5fbb00e888bb7312e02d687652dfc7bd06a5f2
BizCub <bizcubmine@gmail.com>
2026-07-03 20:29:14 +0200

    Updated Gradle dependencies Added issue templates Fix publish versions

--- buildSrc/src/main/kotlin/multiloader.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader.gradle.kts
@@ -1,6 +1,30 @@
 plugins {
     id("me.modmuss50.mod-publish-plugin")
+    id("dev.kikugie.fletching-table")
     id("com.bizcub.multiloader")
 }
 
-multiloader.init()
+multiloader {
+    sc.replacements {
+        string(scp >= "26.2") {
+            replace(".setScreen(", ".gui.setScreen(")
+        }
+        string(scp >= "26.1") {
+            replace(".render(", ".extractRenderState(")
+            replace(".drawString(", ".text(")
+            replace("renderContent", "extractContent")
+            replace("GuiGraphics", "GuiGraphicsExtractor")
+        }
+    }
+
+    if (isFabric) {
+        addDependency(
+            dependency = "net.fabricmc:fabric-loader:${getDep("fabric")}"
+        )
+        addDependency(
+            dependency = "net.fabricmc.fabric-api:fabric-api:${getDep("fabric-api")}",
+            isPublishDepEnabled = true,
+            publishRequirement = "requires"
+        )
+    }
+}
